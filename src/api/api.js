17cc0a44739330be15eea42b2b2330d829dd9cef
admin <admin@DESKTOP-387UJUJ>
2024-06-10 20:48:18 +0300

    finish project2

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://a27668-e049.t.d-f.pw:80/api';
+export const API_URL = 'https://a27670-fb01.t.d-f.pw/api';
 const $api = axios.create({
   baseURL: API_URL,
   headers: {
