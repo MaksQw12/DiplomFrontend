2effadcf028e8069a10823273ec742ef608938ea
admin <admin@DESKTOP-387UJUJ>
2024-06-10 19:59:39 +0300

    test

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5052/api';
+export const API_URL = 'https://a27668-e049.t.d-f.pw:80/api';
 const $api = axios.create({
   baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default $api;
